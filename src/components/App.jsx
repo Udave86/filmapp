84b0f124c4de32ac2d62f31616557934ea78ab7d
Uday Dave <udave86@gmail.com>
2023-07-08 13:13:23 +0530

    structures and css
    Created structures and added styles for intial file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import { Actors, MoiveInformation, Movies, NavBar, Profile } from './index';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const classes = useStyles();
 
-export default App
+  return(
+<div className={classes.root}>
+    <CssBaseline />
+    <NavBar />
+    <main className={classes.content}>
+    <div className={classes.toolbar}/>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <MoiveInformation/>
+        </Route>
+        <Route exact path="/actors/:id">
+          <Actors />
+        </Route>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
+    </div>
+  )
+};
+
+export default App;
 
